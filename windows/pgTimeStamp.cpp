79225abb1b9b5efc101958307c6b8f96cc90186e
jdh88 <johirokawa@gmail.com>
2017-01-10 13:57:30 -0500

    pg_timestamp debugged :cake:

--- windows/pgTimeStamp.cpp
+++ windows/pgTimeStamp.cpp
@@ -1,45 +1,66 @@
 #include "pgTimeStamp.h"
 
-//#include "opencv_headers\opencv\opencv2\highgui\highgui.hpp";
-//#include "opencv_headers\opencv\opencv2\core\core.hpp";
-
 double pg_timestamp(IplImage* frame)
 {
 	double ts = 0.0;
 
-	IplImage* ts_pixels;
-	//cv::Rect roi_rect(0, 0, 4, 1);
-
-	int row = 0;
-	int col = 0;
+	int x = 0;  //column
+	int y = 0;  //row
+	int cchan = 0;  //color channel of interest
 
 	//Grab each pixel's data
-	uint32_t pixel0 = CV_IMAGE_ELEM(frame, unsigned char, row, col+0);
-	uint32_t pixel1 = CV_IMAGE_ELEM(frame, unsigned char, row, col+1);
-	uint32_t pixel2 = CV_IMAGE_ELEM(frame, unsigned char, row, col+2);
-	uint32_t pixel3 = CV_IMAGE_ELEM(frame, unsigned char, row, col+3);
+	uint32_t pixel0 = CV_IMAGE_ELEM(frame, unsigned char, y, ((x+0)*3)+cchan);
+	uint32_t pixel1 = CV_IMAGE_ELEM(frame, unsigned char, y, ((x+1)*3)+cchan);
+	uint32_t pixel2 = CV_IMAGE_ELEM(frame, unsigned char, y, ((x+2)*3)+cchan);
+	uint32_t pixel3 = CV_IMAGE_ELEM(frame, unsigned char, y, ((x+3)*3)+cchan);
+	
+	//printf("pixel values:  %d %d %d %d\n", (int)pixel0, (int)pixel1, (int)pixel2, (int)pixel3);
+	//printbinary(pixel0);
+	//printf(" | ");
+	//printbinary(pixel1);
+	//printf(" | ");
+	//printbinary(pixel2);
+	//printf(" | ");
+	//printbinary(pixel3);
+	//printf("\n");
 
-	printf("pixel values:  %d %d %d %d\n", pixel0, pixel1, pixel2, pixel3);
-
-	//Combine the bits
+	//Shift bits so we can combine
 	pixel0 = pixel0 << (32 - 8);
 	pixel1 = pixel1 << (32 - 16);
 	pixel2 = pixel2 << (32 - 24);
 
+	//printbinary(pixel0);
+	//printf(" | ");
+	//printbinary(pixel1);
+	//printf(" | ");
+	//printbinary(pixel2);
+	//printf(" | ");
+	//printbinary(pixel3);
+	//printf("\n");
+
+	//Combine the bits
 	uint32_t ts_raw = pixel0 | pixel1 | pixel2 | pixel3;
 
-	//Extract bits
-	uint32_t second_count = extract_bits(ts_raw, 7, 32);
-	uint32_t cycle_count = extract_bits(ts_raw, 13, 32 - 7);
-	uint32_t cycle_offset = extract_bits(ts_raw, 12, 32 - 7 - 13);
+	//printbinary(ts_raw);
+	//printf("\n");
 
-	//Shift bits to get actual decimals
-	//second_count = second_count >>
+	//Extract bits (0 is right most bit)
+	uint32_t second_count = extract_bits(ts_raw, 7, 25);  //was 31
+	uint32_t cycle_count = extract_bits(ts_raw, 13, 12);  //was 31-7
+	uint32_t cycle_offset = extract_bits(ts_raw, 12, 0);
+	
+	//printbinary(second_count);
+	//printf(" | ");
+	//printbinary(cycle_count);
+	//printf(" | ");
+	//printbinary(cycle_offset);
+	//printf("\n");
 
-	//cv::Mat roi = frame(roi_rect);
+	//Convert to single ts
+	ts = second_count + (cycle_count + cycle_offset / 3072.0) / 8000.0;
 
-	//cv::imshow("Image", roi);
-	//cv::waitKey();
+	//printf("%d, %d, %d\n", (int)second_count, (int)cycle_count, (int)cycle_offset);
+	//printf("%f\n", ts);
 
 	return ts;
 }
@@ -48,4 +69,12 @@ uint32_t extract_bits(uint32_t num, uint8_t nbits, uint8_t start)
 {
 	uint32_t mask = ((1 << nbits) - 1) << start;  // create mask
 	return (num & mask) >> start;
+}
+
+void printbinary(unsigned n)
+{
+	for (int i = 31; i >= 0; i--)
+	{
+		std::cout << ((n >> i) & 1);
+	}
 }
--- windows/pgTimeStamp.h
+++ windows/pgTimeStamp.h
@@ -3,3 +3,4 @@
 
 double pg_timestamp(IplImage* frame);
 uint32_t extract_bits(uint32_t num, uint8_t nbits, uint8_t start);
+void printbinary(unsigned n);
