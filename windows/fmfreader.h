f1be22ba539d95ffcb09d40df663ffbb06b83b71
jdh88 <johirokawa@gmail.com>
2017-02-13 17:13:12 -0500

    reformatting

--- windows/fmfreader.h
+++ windows/fmfreader.h
@@ -6,16 +6,12 @@
 #include "cv.h"
 
 class fmfreader {
-	/* in the compressed output, how many bits per pixel? */
-	#define BITRATEPERPIXEL 5
-	/* how often to we emit a key frame? */
-	#define STREAM_GOP_SIZE 100
-	/* frames per second */
-	#define STREAM_FRAME_RATE 25 
-	/* color encoding for each pixel; most formats seem to like
-	YUV420 */
-	#define STREAM_PIX_FMT PIX_FMT_YUV420P /* default pix_fmt */
-	/* fmf input */
+	#define BITRATEPERPIXEL 5  /* in the compressed output, how many bits per pixel? */
+	#define STREAM_GOP_SIZE 100	/* how often to we emit a key frame? */
+	#define STREAM_FRAME_RATE 25 	/* frames per second */
+	#define STREAM_PIX_FMT PIX_FMT_YUV420P 	/* color encoding for each pixel; most formats seem to like YUV420 */
+											/* default pix_fmt */
+	
 	typedef unsigned int uint32;
 	typedef unsigned long long uint64;
 
@@ -36,34 +32,24 @@ public:
 	void fmf_pointff(); //point to start of first frame (ie. after header)
 
 private:
-	/* pointer to fmf file stream */
-	FILE * fmffp;
-	/* number of pixels in each frame */
-	int fmfnpixels;
-	/* number of bytes in header */
-	uint64 headersize;
-	/* version of fmf file */
-	uint32 version;
-	/* number or rows of pixels in each frame */
-	uint32 nr;
-	/* number or columns of pixels in each frame */
-	uint32 nc;
-	/* number of bytes to encode each frame */
-	uint64 bytesperchunk;
-	/* number of frames */
-	uint64 nframes;
-	/* number of bits used to encode each pixel */
-	uint32 bitsperpixel;
-	/* 1 if nr is odd, 0 o.w. This is necessary because the
-	video is required to have and even number of rows and
-	columns. We pad one column on the right and one column
-	on th bottom if necessary. */
-	int isoddrows;
-	/* 1 if nc is odd, 0 o.w. */
-	int isoddcols;
-	/* if isoddcols==1, then we will need an extra buffer to
-	read in the unpadded image. */
-	uint8_t * extrabuf;
+
+	FILE * fmffp; 	/* pointer to fmf file stream */
+	int fmfnpixels;	/* number of pixels in each frame */
+	uint64 headersize;	/* number of bytes in header */
+	uint32 version;	/* version of fmf file */
+	uint32 nr;	/* number or rows of pixels in each frame */
+	uint32 nc;	/* number or columns of pixels in each frame */
+	uint64 bytesperchunk;	/* number of bytes to encode each frame */
+	uint64 nframes;	/* number of frames */
+	uint32 bitsperpixel;	/* number of bits used to encode each pixel */
+
+	int isoddrows;	/* 1 if nr is odd, 0 o.w. This is necessary because the
+					video is required to have and even number of rows and
+					columns. We pad one column on the right and one column
+					on th bottom if necessary. */
+	int isoddcols;	/* 1 if nc is odd, 0 o.w. */
+
+	uint8_t * extrabuf;	/* if isoddcols==1, then we will need an extra buffer to read in the unpadded image. */
 
 	//Variables used in buffer conversion to IplImage
 	uint64 currframe;
